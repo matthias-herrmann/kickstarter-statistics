2a30cdae84211969ddd2e2bd4c3cf0c53ea72465
Shoggomo <andy-hot@web.de>
2018-06-01 12:38:26 +0200

    Restructured DataManager and added functions

--- src/DataManager.js
+++ src/DataManager.js
@@ -1,280 +1,75 @@
 import data from './assets/data';
+import countryCodeConversions from './assets/countryISOCodeConversions';
 
-const countryCodeConversions = {
-  "AD": "AND",
-  "AE": "ARE",
-  "AF": "AFG",
-  "AG": "ATG",
-  "AI": "AIA",
-  "AL": "ALB",
-  "AM": "ARM",
-  "AN": "ANT",
-  "AO": "AGO",
-  "AQ": "ATA",
-  "AR": "ARG",
-  "AS": "ASM",
-  "AT": "AUT",
-  "AU": "AUS",
-  "AW": "ABW",
-  "AZ": "AZE",
-  "BA": "BIH",
-  "BB": "BRB",
-  "BD": "BGD",
-  "BE": "BEL",
-  "BF": "BFA",
-  "BG": "BGR",
-  "BH": "BHR",
-  "BI": "BDI",
-  "BJ": "BEN",
-  "BL": "BLM",
-  "BM": "BMU",
-  "BN": "BRN",
-  "BO": "BOL",
-  "BR": "BRA",
-  "BS": "BHS",
-  "BT": "BTN",
-  "BW": "BWA",
-  "BY": "BLR",
-  "BZ": "BLZ",
-  "CA": "CAN",
-  "CC": "CCK",
-  "CD": "COD",
-  "CF": "CAF",
-  "CG": "COG",
-  "CH": "CHE",
-  "CI": "CIV",
-  "CK": "COK",
-  "CL": "CHL",
-  "CM": "CMR",
-  "CN": "CHN",
-  "CO": "COL",
-  "CR": "CRI",
-  "CU": "CUB",
-  "CV": "CPV",
-  "CW": "CUW",
-  "CX": "CXR",
-  "CY": "CYP",
-  "CZ": "CZE",
-  "DE": "DEU",
-  "DJ": "DJI",
-  "DK": "DNK",
-  "DM": "DMA",
-  "DO": "DOM",
-  "DZ": "DZA",
-  "EC": "ECU",
-  "EE": "EST",
-  "EG": "EGY",
-  "EH": "ESH",
-  "ER": "ERI",
-  "ES": "ESP",
-  "ET": "ETH",
-  "FI": "FIN",
-  "FJ": "FJI",
-  "FK": "FLK",
-  "FM": "FSM",
-  "FO": "FRO",
-  "FR": "FRA",
-  "GA": "GAB",
-  "GB": "GBR",
-  "GD": "GRD",
-  "GE": "GEO",
-  "GG": "GGY",
-  "GH": "GHA",
-  "GI": "GIB",
-  "GL": "GRL",
-  "GM": "GMB",
-  "GN": "GIN",
-  "GQ": "GNQ",
-  "GR": "GRC",
-  "GT": "GTM",
-  "GU": "GUM",
-  "GW": "GNB",
-  "GY": "GUY",
-  "HK": "HKG",
-  "HN": "HND",
-  "HR": "HRV",
-  "HT": "HTI",
-  "HU": "HUN",
-  "ID": "IDN",
-  "IE": "IRL",
-  "IL": "ISR",
-  "IM": "IMN",
-  "IN": "IND",
-  "IO": "IOT",
-  "IQ": "IRQ",
-  "IR": "IRN",
-  "IS": "ISL",
-  "IT": "ITA",
-  "JE": "JEY",
-  "JM": "JAM",
-  "JO": "JOR",
-  "JP": "JPN",
-  "KE": "KEN",
-  "KG": "KGZ",
-  "KH": "KHM",
-  "KI": "KIR",
-  "KM": "COM",
-  "KN": "KNA",
-  "KP": "PRK",
-  "KR": "KOR",
-  "KW": "KWT",
-  "KY": "CYM",
-  "KZ": "KAZ",
-  "LA": "LAO",
-  "LB": "LBN",
-  "LC": "LCA",
-  "LI": "LIE",
-  "LK": "LKA",
-  "LR": "LBR",
-  "LS": "LSO",
-  "LT": "LTU",
-  "LU": "LUX",
-  "LV": "LVA",
-  "LY": "LBY",
-  "MA": "MAR",
-  "MC": "MCO",
-  "MD": "MDA",
-  "ME": "MNE",
-  "MF": "MAF",
-  "MG": "MDG",
-  "MH": "MHL",
-  "MK": "MKD",
-  "ML": "MLI",
-  "MM": "MMR",
-  "MN": "MNG",
-  "MO": "MAC",
-  "MP": "MNP",
-  "MR": "MRT",
-  "MS": "MSR",
-  "MT": "MLT",
-  "MU": "MUS",
-  "MV": "MDV",
-  "MW": "MWI",
-  "MX": "MEX",
-  "MY": "MYS",
-  "MZ": "MOZ",
-  "NA": "NAM",
-  "NC": "NCL",
-  "NE": "NER",
-  "NG": "NGA",
-  "NI": "NIC",
-  "NL": "NLD",
-  "NO": "NOR",
-  "NP": "NPL",
-  "NR": "NRU",
-  "NU": "NIU",
-  "NZ": "NZL",
-  "OM": "OMN",
-  "PA": "PAN",
-  "PE": "PER",
-  "PF": "PYF",
-  "PG": "PNG",
-  "PH": "PHL",
-  "PK": "PAK",
-  "PL": "POL",
-  "PM": "SPM",
-  "PN": "PCN",
-  "PR": "PRI",
-  "PS": "PSE",
-  "PT": "PRT",
-  "PW": "PLW",
-  "PY": "PRY",
-  "QA": "QAT",
-  "RE": "REU",
-  "RO": "ROU",
-  "RS": "SRB",
-  "RU": "RUS",
-  "RW": "RWA",
-  "SA": "SAU",
-  "SB": "SLB",
-  "SC": "SYC",
-  "SD": "SDN",
-  "SE": "SWE",
-  "SG": "SGP",
-  "SH": "SHN",
-  "SI": "SVN",
-  "SJ": "SJM",
-  "SK": "SVK",
-  "SL": "SLE",
-  "SM": "SMR",
-  "SN": "SEN",
-  "SO": "SOM",
-  "SR": "SUR",
-  "SS": "SSD",
-  "ST": "STP",
-  "SV": "SLV",
-  "SX": "SXM",
-  "SY": "SYR",
-  "SZ": "SWZ",
-  "TC": "TCA",
-  "TD": "TCD",
-  "TG": "TGO",
-  "TH": "THA",
-  "TJ": "TJK",
-  "TK": "TKL",
-  "TL": "TLS",
-  "TM": "TKM",
-  "TN": "TUN",
-  "TO": "TON",
-  "TR": "TUR",
-  "TT": "TTO",
-  "TV": "TUV",
-  "TW": "TWN",
-  "TZ": "TZA",
-  "UA": "UKR",
-  "UG": "UGA",
-  "US": "USA",
-  "UY": "URY",
-  "UZ": "UZB",
-  "VA": "VAT",
-  "VC": "VCT",
-  "VE": "VEN",
-  "VG": "VGB",
-  "VI": "VIR",
-  "VN": "VNM",
-  "VU": "VUT",
-  "WF": "WLF",
-  "WS": "WSM",
-  "XK": "XKX",
-  "YE": "YEM",
-  "YT": "MYT",
-  "ZA": "ZAF",
-  "ZM": "ZMB",
-  "ZW": "ZWE",
+export const convertCountryCode = (code) => {
+  return countryCodeConversions[code] || "404"
 };
 
-const convertCountryCode = code => countryCodeConversions[code] || "404";
-
-const dataSetToCountryObject = dataSet => ({
-  [dataSet['country']]: {
-    ...dataSet
+export const dataSetToCountryObject = (dataSet) => {
+  return {
+    [dataSet['country']]: {
+      ...dataSet
+    }
   }
-});
+};
 
 /**
  * Returns all datasets as a list
  * @returns {{country: *}[]}
  */
-export const dataList = () => Object.values(data).map(e => ({
-  ...e,
-  'country': convertCountryCode(e['country']),
-}));
+export const dataList = () => {
+  return Object.values(data).map(e => ({
+    ...e,
+    'country': convertCountryCode(e['country']),
+  }))
+};
 
 /**
  * Generates a object with country codes as keys
  * @returns {*} Object in the form of {"countrycode": {data...}, ...}
  */
-export const countryObject = () => asList().reduce((obj, dataSet) => ({
-  ...obj,
-  ...dataSetToCountryObject(dataSet),
-}), {});
+export const countryObject = () => {
+  return asList().reduce((obj, dataSet) => ({
+    ...obj,
+    ...dataSetToCountryObject(dataSet),
+  }), {});
+};
 
 /**
  * Generates colors for all countries
  * @param colorFunction The function to convert the dataset to a color.
  * @returns {*} A object in the form of {"countrycode": color, ...}
  */
-export const countryToColors = colorFunction => dataList().reduce((obj, dataSet) => ({
-  ...obj,
-  [dataSet["country"]]: colorFunction(dataSet),
-}), {});
+export const countryToColors = (colorFunction) => {
+  return dataList().reduce((obj, dataSet) => ({
+    ...obj,
+    [dataSet["country"]]: colorFunction(dataSet),
+  }), {});
+};
+
+export const getValueList = (key) => {
+  return dataList().map(dataSet => dataSet[key]);
+};
+
+export const percentile = (key, percent) => {
+  const values = getValueList(key).sort(),
+    index = Math.floor(values.length * percent);
+  return values[index];
+};
+
+export const min = (key) => {
+  return Math.min(...getValueList(key));
+};
+
+export const max = (key) => {
+  return Math.max(...getValueList(key));
+};
+
+export const median = (key) => {
+  return percentile(key, 0.5);
+};
+
+export const avg = (key) => {
+  const values = getValueList(key);
+  return values.reduce((prev, curr) => prev + curr, 0) / values.length;
+};
