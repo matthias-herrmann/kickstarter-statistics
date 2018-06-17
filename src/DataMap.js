0201e36d42348d8c8169504b383ed4c2d1da7fa0
Shoggomo <andy-hot@web.de>
2018-06-17 12:29:20 +0200

    Bugfix

--- src/DataMap.js
+++ src/DataMap.js
@@ -12,86 +12,86 @@ import Fill from 'ol/style/fill';
 import control from 'ol/control';
 import Control from 'ol/control/control';
 import * as db from './DataManager';
-import * as d3Scale from "d3-scale";
+import * as d3Scale from 'd3-scale';
 
 //todo map legend
 
 export const createColorScale = (key, colors) => {
-  const min = db.min(key),
-    max = db.max(key),
-    domain = [min, max];
-  return d3Scale.scaleQuantize()
-    .domain(domain)
-    .range(colors);
+	const min = db.min(key),
+		max = db.max(key),
+		domain = [min, max];
+	return d3Scale.scaleQuantize()
+		.domain(domain)
+		.range(colors);
 };
 
 export const getCountryColors = (colorScale, key) => {
-  return db.countryToColors(country => colorScale(country[key]));
+	return db.countryToColors(country => colorScale(country[key]));
 };
 
 export const createVectorStyleFunction = countryColors => feature => new Style({
-  stroke: new Stroke({
-    color: 'cornflowerblue',
-    width: 1.5,
-  }),
-  fill: new Fill({
-    color: countryColors[feature.id_] || "white"
-  })
+	stroke: new Stroke({
+		color: 'cornflowerblue',
+		width: 1.5,
+	}),
+	fill: new Fill({
+		color: countryColors[feature.id_] || 'white'
+	})
 });
 
 export const createNewVectorLayer = () => new Vector({
-  source: new VectorSource({
-    url: './assets/countries.geojson',
-    format: new GeoJSONFormat(),
-  }),
+	source: new VectorSource({
+		url: './assets/countries.geojson',
+		format: new GeoJSONFormat(),
+	}),
 });
 
 export const drawNewMap = (target, layers) => new Map({
-  layers: [
-    layers,
-  ],
-  target: "map",
-  view: new View({
-    center: proj.fromLonLat([13.41, 52.52]), //Germany
-    zoom: 4, //zoom constant
-    maxZoom: 5,
-    minZoom: 2,
-    extent: [-10018254, -10018254, 10018254, 10018254],
-  }),
-  controls: control.defaults({
-    rotate: false,
-    attribution: false,
-  }),
+	layers: [
+		layers,
+	],
+	target: 'map',
+	view: new View({
+		center: proj.fromLonLat([13.41, 52.52]), //Germany
+		zoom: 4, //zoom constant
+		maxZoom: 5,
+		minZoom: 2,
+		extent: [-10018254, -10018254, 10018254, 10018254],
+	}),
+	controls: control.defaults({
+		rotate: false,
+		attribution: false,
+	}),
 });
 
 export const calcMapColors = (key, colorRange) => {
-  const colorScale = createColorScale(key, colorRange);
-  return getCountryColors(colorScale, key);
+	const colorScale = createColorScale(key, colorRange);
+	return getCountryColors(colorScale, key);
 };
 
 export const setMapColors = (countryColors, layer) => {
-  layer.setStyle(createVectorStyleFunction(countryColors))
+	layer.setStyle(createVectorStyleFunction(countryColors));
 };
 
 //todo better name, rework!!
 export const createButtonControls = (textKeys, colors, layer) => {
 
-  const controlsContainer = document.createElement('div');
-  controlsContainer.className = "controls-container ol-unselectable";
+	const controlsContainer = document.createElement('div');
+	controlsContainer.className = 'controls-container ol-unselectable';
 
-  Object.keys(textKeys).forEach(key => {
-    const container = document.createElement('div'),
-      button = document.createElement('button');
-    button.innerHTML = textKeys[key];
-    button.addEventListener('click', () => {
-      const colorScale = createColorScale(key, colors),
-        countryColors = getCountryColors(colorScale, key);
-      setMapColors(countryColors, layer);
-    });
-    container.className = 'category-container ol-control';
-    container.appendChild(button);
-    controlsContainer.appendChild(container);
-  });
+	Object.keys(textKeys).forEach(key => {
+		const container = document.createElement('div'),
+			button = document.createElement('button');
+		button.innerHTML = textKeys[key];
+		button.addEventListener('click', () => {
+			const colorScale = createColorScale(key, colors),
+				countryColors = getCountryColors(colorScale, key);
+			setMapColors(countryColors, layer);
+		});
+		container.className = 'category-container ol-control';
+		container.appendChild(button);
+		controlsContainer.appendChild(container);
+	});
 
-  return new Control({element: controlsContainer});
+	return new Control({element: controlsContainer});
 };
--- src/main.js
+++ src/main.js
@@ -1,36 +1,36 @@
 import './styles/stylesheet.css';
 import 'ol/ol.css';
-import * as DataMap from "./DataMap";
-import P from "./popupMain";
+import * as DataMap from './DataMap';
+import P from './popupMain';
 
 const setMapColors = (colors, key, layer) => {
-  const countryColors = DataMap.calcMapColors(key, colors);
-  DataMap.setMapColors(countryColors, layer);
+	const countryColors = DataMap.calcMapColors(key, colors);
+	DataMap.setMapColors(countryColors, layer);
 };
 
 const addMapControls = (map, layer, colors) => {
-  map.addControl(DataMap.createButtonControls({
-    "avg_pledged": "Pledged avg",
-    "median_pledged": "Pledged median",
-    "avg_goal": "Goal avg",
-    "median_goal": "Goal median",
-    "projects_count": "Count",
-  }, colors, layer));
+	map.addControl(DataMap.createButtonControls({
+		'avg_pledged': 'Pledged avg',
+		'median_pledged': 'Pledged median',
+		'avg_goal': 'Goal avg',
+		'median_goal': 'Goal median',
+		'projects_count': 'Count',
+	}, colors, layer));
 };
 
 const initMap = () => {
-  const target = "map",
-    colors = ["#F4E4D8", "#EAC7B0", "#DFAC88", "#D48E5F", "#C97234", "#A15A28", "#79421B", "#4F290E", "#231003"],
-    vecLayer = DataMap.createNewVectorLayer(),
-    map = DataMap.drawNewMap(target, vecLayer);
-  addMapControls(map, colors, vecLayer);
-  setMapColors(colors, "avg_pledged", vecLayer);
-  return map;
+	const target = 'map',
+		colors = ['#F4E4D8', '#EAC7B0', '#DFAC88', '#D48E5F', '#C97234', '#A15A28', '#79421B', '#4F290E', '#231003'],
+		vecLayer = DataMap.createNewVectorLayer(),
+		map = DataMap.drawNewMap(target, vecLayer);
+	addMapControls(map, vecLayer, colors);
+	setMapColors(colors, 'avg_pledged', vecLayer);
+	return map;
 };
 
 const init = () => {
-  const olMap = initMap();
-  new P(olMap);
+	const olMap = initMap();
+	new P(olMap);
 };
 
 init();
