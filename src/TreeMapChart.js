25c41b3358e9d6f3a07cde5674f388e71e7d6b28
Matthias Herrmann <mherrmann@DESKTOP-DD2O9L4.fritz.box>
2018-06-30 23:07:29 +0200

    Text in center during transition with setInterval function

--- src/TreeMapChart.js
+++ src/TreeMapChart.js
@@ -5,21 +5,20 @@ import data from './assets/officialKickstarterAggregated';
 const svg = d3.select('svg');
 const width = parseFloat(svg.attr('width'));
 const height = parseFloat(svg.attr('height'));
-const color = d3.scaleOrdinal(['#49c8c5',
-	'#e57de2',
-	'#58ca67',
-	'#d499e4',
-	'#93c23d',
-	'#76a2f1',
-	'#e3a438',
-	'#53bae9',
-	'#bab74f',
-	'#aca5e7',
-	'#78c388',
-	'#ec8cae',
-	'#cdb074',
-	'#a3c9fe',
-	'#ef8f6c']);
+const color = d3.scaleOrdinal(['#9f3857',
+	'#01b92f',
+	'#354ed4',
+	'#cbae09',
+	'#c38dff',
+	'#c87800',
+	'#006aa9',
+	'#ae3b00',
+	'#00c19b',
+	'#ff557a',
+	'#586222',
+	'#b3a9dc',
+	'#825044',
+	'#f59688']);
 const format = d3.format(',d');
 
 const sumByCount = (d) => d.count;
@@ -32,6 +31,14 @@ const treemap = d3.treemap()
 	.size([width, height])
 	.round(true)
 	.paddingInner(1);
+
+const calculateCenterOfTextInRectangle = (tspanNode) => {
+	const rectangleNode = tspanNode.parentElement.previousSibling.previousSibling; // tspan is in text element and 2 nodes above is the rectangle
+	const centerX = (rectangleNode.getAttribute('width') / 2) - (tspanNode.getComputedTextLength() / 2);
+	const centerY = (rectangleNode.getAttribute('height') / 2) + (19 / 2); // 19 is the font-size in pixel
+	return {centerX: centerX,
+		centerY: centerY};
+};
 
 const root = d3.hierarchy(data)
 	.eachBefore((d) => { d.data.id = d.data.name; })
@@ -63,10 +70,12 @@ cell.append('text')
 	.selectAll('tspan')
 	.data((d) => d.data.name.split(/(?=[A-Z](\s|&)+[^A-Z])/g))
 	.enter().append('tspan') // within text element, text and font properties and the current position
-	.attr('x', 4)			  // can be adjusted with absolute or relative coordinate values
-	.attr('y', (d, i) => 13 + i * 10)
 	.attr('fill', 'whitesmoke')
 	.text((d) => d);
+
+cell.selectAll('tspan')
+	.attr('x', function() { return calculateCenterOfTextInRectangle(this).centerX; }) // center x and y. Not using Es6 function because of this context which is the tspan element.
+	.attr('y', function() { return calculateCenterOfTextInRectangle(this).centerY; });
 
 cell.append('title') // Getting displayed on hover
 	.text((d) => titleFromDataObject(d));
@@ -83,17 +92,29 @@ const timeout = d3.timeout(() => {
 	d3.select('input[value="sumByTotalDollars"]')
 		.property('checked', true)
 		.dispatch('change');
-}, 2000);
+}, 10000);
 
 function changed(sum) { // function object: e.g. sumByCount, sumBySize, sumByTotalDollars
 	timeout.stop();
+
+	const intervalId = setInterval(
+		() => {
+			cell.selectAll('tspan')
+				.attr('x', function() { return calculateCenterOfTextInRectangle(this).centerX; }) // center x and y. Not using Es6 function because of this context which is the tspan element.
+				.attr('y', function() { return calculateCenterOfTextInRectangle(this).centerY; });
+		},
+		0.03
+	);
+
 	treemap(root.sum(sum));
 	cell.transition()
 		.duration(750)
 		.attr('transform', (d) => 'translate(' + d.x0 + ',' + d.y0 + ')')
-		.select('rect')
+		.selectAll('rect')
 		.attr('width', (d) => d.x1 - d.x0)
-		.attr('height', (d) => d.y1 - d.y0);
+		.attr('height', (d) => d.y1 - d.y0)
+		.each('end', function() {intervalId.stop();});
+
 	cell.selectAll('title')
 		.text(d => titleFromDataObject(d));
 }
