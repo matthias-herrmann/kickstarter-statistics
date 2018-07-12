ed16ec91ff993b420ac30b2968c7a01b7f28be05
Shoggomo <andy-hot@web.de>
2018-07-12 13:16:05 +0200

    Removed median values

--- src/main.js
+++ src/main.js
@@ -8,11 +8,9 @@ const initMap = () => {
 	const target = 'map',
 		colors = ['#F4E4D8', '#EAC7B0', '#DFAC88', '#D48E5F', '#C97234', '#A15A28', '#79421B', '#4F290E', '#231003'],
 		keyValues = {		// needs better name
-			'avg_pledged': 'Pledged avg',
-			'median_pledged': 'Pledged median',
-			'avg_goal': 'Goal avg',
-			'median_goal': 'Goal median',
-			'projects_count': 'Count',
+			'avg_pledged': 'Pledged',
+			'avg_goal': 'Goal',
+			'projects_count': 'Project Count',
 		};
 	return DataMap.createDataMap(target, colors, keyValues);
 };
