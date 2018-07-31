4944ce153187c0331ca898af945f080171c746a2
Matthias Herrmann <mherrmann@DESKTOP-DD2O9L4.fritz.box>
2018-07-31 17:30:00 +0200

    Fixed arrow functionality

--- src/MoreContentArrow.js
+++ src/MoreContentArrow.js
@@ -1,18 +1,43 @@
-import * as d3 from 'd3';
-const arrowContainer = document.querySelector('.arrow-container');
-const angleDown = document.querySelector('.fa-angle-down');
+const arrowIcon = document.querySelector('.arrow-2').children[0];
 const scrollToBottom = () => window.scrollTo(0, document.body.scrollHeight);
 const scrollToTop = () => window.scrollTo(0, 0);
-let arrowPosition = 'upwards';
+const arrowContainer = document.querySelector('.arrow-container');
+const VIEW_PORT_PERCENTAGE_SHOULD_ROTATE_ANGLE = 0.6;
 
-arrowContainer.addEventListener('click', () => {
-	// trigger arrow animaton
-	angleDown.classList.remove(arrowPosition);
-	arrowPosition = arrowPosition === 'upwards' ? 'downwards' : 'upwards';
-	if(arrowPosition === 'upwards') {
-		scrollToBottom();
-	} else  {
+arrowContainer.addEventListener('click', function() {
+	if(isArrowPointingUpwards()) {
 		scrollToTop();
+	} else {
+		scrollToBottom();
 	}
-	angleDown.classList.add(arrowPosition);
 });
+
+// https://stackoverflow.com/a/49989567/5111904
+window.addEventListener('scroll', function() {
+	let yOffset = this.scrollY;
+	let innerHeight = window.innerHeight;
+
+	if(yOffset > VIEW_PORT_PERCENTAGE_SHOULD_ROTATE_ANGLE * innerHeight) {
+		pointArrowUpwards();
+	} else {
+		pointArrowDownwards();
+	}
+});
+
+function isArrowPointingUpwards() {
+	return arrowIcon.classList.contains('fa-angle-up');
+}
+
+function pointArrowUpwards() {
+	if(arrowIcon.classList.contains('fa-angle-down')) {
+		arrowIcon.classList.remove('fa-angle-down');
+		arrowIcon.classList.add('fa-angle-up');
+	}
+}
+
+function pointArrowDownwards() {
+	if(arrowIcon.classList.contains('fa-angle-up')) {
+		arrowIcon.classList.remove('fa-angle-up');
+		arrowIcon.classList.add('fa-angle-down');
+	}
+}
